192a6651e577314dd53667b493a4c1221e3993cf
Benzoh <benzoh.wada@gmail.com>
2021-05-07 21:34:38 +0900

    WIP: author

--- public/_cms/wp-content/themes/hippoblog_v2/components/molecules/entries.php
+++ public/_cms/wp-content/themes/hippoblog_v2/components/molecules/entries.php
@@ -3,7 +3,16 @@
 foreach ($args as $entry) {
   // _D($entry);
 
-  get_template_part('components/atoms/thumbnail', null, []);
+  // author取得
+  $author_slug = get_the_author_meta('user_nicename');
+  $author_archive_url = get_author_posts_url($author_slug) . $author_slug;
+  $author = get_posts([
+    'name' => $author_slug,
+    'post_type' => 'profile',
+  ]);
+
+  get_template_part('components/atoms/thumbnail', null, ['post_thumbnail' => wp_get_attachment_image_src(get_post_thumbnail_id($entry), 'full')]);
+  get_template_part('components/atoms/author', null, []);
   get_template_part('components/atoms/title', null, ['post_title' => $entry->post_title]);
   get_template_part('components/atoms/data', null, []);
   get_template_part('components/atoms/button', null, []);
