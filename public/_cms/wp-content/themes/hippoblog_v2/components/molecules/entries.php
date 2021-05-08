83a218c78e0fc65c3726a4aef60b02eb5fb52c11
Benzoh <benzoh.wada@gmail.com>
2021-05-08 21:34:48 +0900

    add date component

--- public/_cms/wp-content/themes/hippoblog_v2/components/molecules/entries.php
+++ public/_cms/wp-content/themes/hippoblog_v2/components/molecules/entries.php
@@ -15,6 +15,5 @@ foreach ($args as $entry) {
   get_template_part('components/atoms/thumbnail', null, ['post_thumbnail' => wp_get_attachment_image_src(get_post_thumbnail_id($entry), 'full')]);
   get_template_part('components/atoms/author', null, $author);
   get_template_part('components/atoms/title', null, ['post_title' => $entry->post_title]);
-  get_template_part('components/atoms/data', null, []);
-  get_template_part('components/atoms/button', null, []);
+  get_template_part('components/atoms/date', null, ['post_date' => $entry->post_date]);
 }
