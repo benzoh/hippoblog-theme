80ceadab7bed8ade8300bcd9b810977101c6daa4
Benzoh <benzoh.wada@gmail.com>
2021-05-08 21:18:04 +0900

    add author component

--- public/_cms/wp-content/themes/hippoblog_v2/components/atoms/author.php
+++ public/_cms/wp-content/themes/hippoblog_v2/components/atoms/author.php
@@ -1,3 +1,10 @@
 <?php
+// _D($args);
+?>
 
-_D($args['post_author'])
+<div class="">
+  <a href="https://twitter.com/<?php echo $args['slug']; ?>" target="_blank">
+    <img src="<?php echo CFS()->get('icon', $args['profile'][0]->ID); ?>" alt="" width="30">
+    <span><?php echo $args['profile'][0]->post_title; ?></span>
+  </a>
+</div>
--- public/_cms/wp-content/themes/hippoblog_v2/components/molecules/entries.php
+++ public/_cms/wp-content/themes/hippoblog_v2/components/molecules/entries.php
@@ -2,17 +2,18 @@
 
 foreach ($args as $entry) {
   // _D($entry);
+  // exit;
 
   // author取得
-  $author_slug = get_the_author_meta('user_nicename');
-  $author_archive_url = get_author_posts_url($author_slug) . $author_slug;
-  $author = get_posts([
-    'name' => $author_slug,
+  $author = [];
+  $author['slug'] = get_the_author_meta('user_nicename', $entry->post_author);
+  $author['profile'] = get_posts([
+    'name' => $author['slug'],
     'post_type' => 'profile',
   ]);
 
   get_template_part('components/atoms/thumbnail', null, ['post_thumbnail' => wp_get_attachment_image_src(get_post_thumbnail_id($entry), 'full')]);
-  get_template_part('components/atoms/author', null, []);
+  get_template_part('components/atoms/author', null, $author);
   get_template_part('components/atoms/title', null, ['post_title' => $entry->post_title]);
   get_template_part('components/atoms/data', null, []);
   get_template_part('components/atoms/button', null, []);
