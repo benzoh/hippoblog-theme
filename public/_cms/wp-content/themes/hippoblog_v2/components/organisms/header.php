f33b117a7686b7693aa0ec558a2843f742441cd2
Benzoh <benzoh.wada@gmail.com>
2021-05-10 14:51:51 +0900

    add Bootstrap Icons

--- public/_cms/wp-content/themes/hippoblog_v2/components/organisms/header.php
+++ public/_cms/wp-content/themes/hippoblog_v2/components/organisms/header.php
@@ -1,7 +1,11 @@
-<?php
-echo __FILE__;
-?>
-
 <header class="">
-  header
+  <div class="container">
+    <div class="row">
+      <div class="col">logo</div>
+      <div class="col-auto">search</div>
+      <div class="col-auto js_menu-btn">menu</div>
+    </div>
+  </div>
 </header>
+<i class="bi-alarm"></i>
+<i class="bi-alarm" style="font-size: 2rem; color: cornflowerblue;"></i>
